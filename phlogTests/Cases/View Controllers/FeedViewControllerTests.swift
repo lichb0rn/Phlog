823ceb6f67b8c30db0c26acf2f5dd2f1c5f11ea0
Miroslav Taleiko <mtaleiko@gmail.com>
2022-04-09 09:59:10 +0300

    Add FeedViewController tests

---
+++ phlogTests/Cases/View Controllers/FeedViewControllerTests.swift
@@ -0,0 +1,68 @@
+//
+//  FeedViewControllerTests.swift
+//  phlogTests
+//
+//  Created by Miroslav Taleiko on 09.04.2022.
+//
+
+import XCTest
+@testable import phlog
+
+class FeedViewControllerTests: XCTestCase {
+
+    var sut: FeedViewContoller!
+    var phlogManager: PhlogManager!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        sut = FeedViewContoller.instantiate(from: .feed)
+        let mockCoreData = MockCoreDataStack()
+        phlogManager = PhlogManager(db: mockCoreData)
+        sut.phlogManager = phlogManager
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        phlogManager = nil
+        try super.tearDownWithError()
+    }
+
+    
+    func whenDidAppear() {
+        sut.viewDidAppear(false)
+    }
+    
+    func whenBackendHasPhlogs() {
+        let images = testingImages()
+        let context = phlogManager.mainContext
+        images.forEach { key, value in
+            let phlog = phlogManager.newPhlog(context: context)
+            let picture = phlogManager.newPicture(with: key, context: context)
+            picture.pictureData = value.pngData()
+            phlog.picture = picture
+            phlog.pictureThumbnail = value.resizeTo(size: CGSize(width: 40, height: 40))?.pngData()
+            
+        }
+        phlogManager.saveChanges()
+    }
+    
+    
+    func testController_whenFreshLoaded_collectionViewIsEmpty() {
+        whenDidAppear()
+        
+        let items = sut.collectionView.numberOfItems(inSection: 0)
+        
+        XCTAssertEqual(items, 0)
+    }
+    
+    
+    func testController_whenLoadedWithData_collectionViewIsNotEmpty() {
+        whenBackendHasPhlogs()
+        whenDidAppear()
+        
+        let itemsInCollectionView = sut.collectionView.numberOfItems(inSection: 0)
+        XCTAssertNotEqual(itemsInCollectionView, 0)
+    }
+}
